ef32472af96d364604210e146bbfe8ea4ad2b14b
Raimguzhinov <d.raimguzhinov@stud.sibguti.ru>
2023-03-27 01:06:14 +0700

    Move ForcePropertyChange

--- SimplePaint/Models/ShapeListBoxItem.cs
+++ SimplePaint/Models/ShapeListBoxItem.cs
@@ -1,5 +1,7 @@
-﻿using ReactiveUI;
+﻿using System.ComponentModel;
+using ReactiveUI;
 using System.Reactive;
+using System.Runtime.CompilerServices;
 
 namespace SimplePaint.Models
 {
@@ -10,9 +12,15 @@ namespace SimplePaint.Models
             Name = name;
             Remove = ReactiveCommand.Create<Unit, Unit>(_ => { map.Remove(this); return new Unit(); });
         }
-
         public string Name { get; }
-
         public ReactiveCommand<Unit, Unit> Remove { get; }
+    }
+    public class ForcePropertyChange : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void UpdProperty([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
